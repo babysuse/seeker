bab1678ffa8a96251da9f3e8813655fd89ae538b
Yusung Lin <yusung.lin@bloomreach.com>
2022-09-27 23:56:39 -0400

    Reorganize the code with OOP
    * Add CLI options
    * Improve refreshing mechanism
    * Add type annotations
    * Add other error case

--- tic-bot.py
+++ tic-bot.py
@@ -1,58 +1,155 @@
+from datetime import datetime
+from secrets import choice
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
-from webdriver_manager.chrome import ChromeDriverManager
-from datetime import datetime
 from time import sleep
+from webdriver_manager.chrome import ChromeDriverManager
+import argparse
 import json
+import random
+import sys
 
-month = datetime.now().month
-day = datetime.now().day
-url = f"https://www.supersaas.com/schedule/jcbc?view=week&day={day}&month={month}"
-slot = "Saturday 6:30pm"
-slot_backup = "Sunday 6:30pm"
 
-with open("credentials.json", encoding="utf-8") as credentials_file:
-    credentials = json.load(credentials_file)
+chrome: webdriver.Chrome = None
+quiet: bool = False
+retry: int = 10  # refresh time before giving up
 
-service = Service(executable_path=ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service)
-driver.implicitly_wait(3)
-driver.get(url)
+def setup_conn() -> None:
+    global chrome
+    month = datetime.now().month
+    day = datetime.now().day
+    url = f'https://www.supersaas.com/schedule/jcbc?view=week&day={day}&month={month}'
 
-# log in and wait until 21:00 (wait for 0.5 sec in case the server isn't ready yet)
-driver.find_element(By.CSS_SELECTOR, "li:nth-child(1)").click()
-driver.find_element(By.ID, "name").send_keys(credentials["username"])
-driver.find_element(By.ID, "password").send_keys(credentials["password"])
-driver.find_element(By.CSS_SELECTOR, "button[name='button']").click()
-print("Waiting for 9pm")
-while True:
-    if datetime.now().strftime("%H") >= "21":
-        sleep(0.5)
-        break
+    service = Service(executable_path=ChromeDriverManager().install())
+    chrome = webdriver.Chrome(service=service)
+    chrome.implicitly_wait(3)
+    chrome.get(url)
 
-# party's up
-print("Party's up")
-driver.refresh()
 
-try:
-    driver.find_element(By.CSS_SELECTOR, f"div[title='{slot}']").click()
-except NoSuchElementException:
-    driver.find_element(By.CSS_SELECTOR, f"div[title='{slot_backup}']").click()
-    print("Saturday's not open! Picked Sunday instead! =3=")
+def reserve(take_humiliation: bool, credentials: dict) -> None:
+    """Fill in player's info and reserve the slot."""
+    try:
+        chrome.find_element(By.ID, "bbox_new").click()
+    except ElementNotInteractableException:
+        if not take_humiliation:
+            log('Too late! Try next time! Q_Q', file=sys.stderr)
+            exit(1)
 
-try:
-    driver.find_element(By.ID, "bbox_new").click()
-except ElementNotInteractableException:
-    driver.find_element(By.ID, "bbox_wait").click()
-    print("Too late! Got into waiting list! Q_Q")
+    try:
+        chrome.find_element(By.ID, 'bbox_wait').click()
+        chrome.find_element(By.CSS_SELECTOR, 'button[name="button"]').click()
 
-driver.find_element(By.CSS_SELECTOR, "button[name='button']").click()
+        chrome.find_element(By.ID, 'form_7').send_keys(credentials['player1'])
+        chrome.find_element(By.ID, 'form_10').send_keys(credentials['player2'])
+        chrome.find_element(By.ID, 'form_13').send_keys(credentials['player3'])
+        chrome.find_element(By.ID, 'form_16').send_keys(credentials['player4'])
+        chrome.find_element(By.CSS_SELECTOR, "input[value='Submit']").click()
+    except:
+        log('Not enough credit to create a new booking!', file=sys.stderr)
+    log('Too late! Added to waiting list! Q_Q')
 
-driver.find_element(By.ID, "form_7").send_keys(credentials["player1"])
-driver.find_element(By.ID, "form_10").send_keys(credentials["player2"])
-driver.find_element(By.ID, "form_13").send_keys(credentials["player3"])
-driver.find_element(By.ID, "form_16").send_keys(credentials["player4"])
-driver.find_element(By.CSS_SELECTOR, "input[value='Submit']").click()
-print(f"Enjoy your day on {slot.split()[0]}! ;)")
+
+def try_secondary(slot: str) -> str:
+    """Get backup slot."""
+    try:
+        chrome.find_element(By.CSS_SELECTOR, f'div[title="{slot}"]').click()
+    except NoSuchElementException:
+        return ''
+    return slot
+
+
+def try_primary(slot: str, backup_slot: str) -> str:
+    """Get primary slot."""
+    try:
+        chrome.find_element(By.CSS_SELECTOR, f'div[title="{slot}"]').click()
+    except NoSuchElementException:
+        return try_secondary(backup_slot)
+    return slot
+
+
+def get_slot(slot: str, slot_backup: str) -> bool:
+    """Refresh the page with random back off in case being banned for requesting too aggressive."""
+    global retry
+    log("Party's up")
+    result: bool = False
+    while retry > 0 and not result:
+        chrome.refresh()
+        result = try_primary(slot, slot_backup)
+        retry -= 1
+        sleep(random.random())
+    return result
+
+
+def login(credentials: dict):
+    chrome.find_element(By.CSS_SELECTOR, 'li:nth-child(1)').click()
+    chrome.find_element(By.ID, 'name').send_keys(credentials['username'])
+    chrome.find_element(By.ID, 'password').send_keys(credentials['password'])
+    chrome.find_element(By.CSS_SELECTOR, 'button[name="button"]').click()
+
+
+def parse_args() -> None:
+    """Parse CLI options."""
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     description=
+"""
+A CLI tool to help reserve JCBC slot. Make sure to set the credentials.json properly before the executing tic-bot.py. Run it before 9pm and the program will do the rest when the starter pistol is fired.
+""",
+                                     epilog=
+"""
+Example usage:
+    pipenv run python tic-bot --slot "Saturday 11am" --backup-slot "Friday 7:30pm"
+"""
+                                     )
+    parser.add_argument('--slot',
+                        default='Saturday 6:30pm',
+                        choices=['Friday 6pm', 'Friday 7:30pm', 'Friday 9pm', 'Friday 10:30pm', 'Saturday 11am', 'Saturday 12:30pm', 'Saturday 2pm', 'Saturday 3:30pm', 'Saturday 5pm', 'Saturday 6:30pm', 'Saturday 8pm'],
+                        help='Slot to be reserved. Valid options are "Friday 6pm", "Friday 7:30pm", "Friday 9pm", "Friday 10:30pm", "Saturday 11am", "Saturday 12:30pm", "Saturday 2pm", "Saturday 3:30pm", "Saturday 5pm", "Saturday 6:30pm", "Saturday 8pm". Default to "Saturday 6:30pm".')
+    parser.add_argument('--backup-slot',
+                        default='Saturday 6:30pm',
+                        choices=['Friday 6pm', 'Friday 7:30pm', 'Friday 9pm', 'Friday 10:30pm', 'Sunday 11am', 'Sunday 12:30pm', 'Sunday 2pm', 'Sunday 3:30pm', 'Sunday 5pm', 'Sunday 6:30pm'],
+                        help='Backup slot to be reserved in case it closes on Saturday. Slots on Sunday are "Sunday 11am", "Sunday 12:30pm", "Sunday 2pm", "Sunday 3:30pm", "Sunday 5pm", "Sunday 6:30pm".')
+    parser.add_argument('--quiet',
+                        action='store_true',
+                        default=False,
+                        help='If provided, suppress the STDOUT message.')
+    parser.add_argument('--queue',
+                        action='store_true',
+                        default=False,
+                        help='If provided, if failed to get a slot, join the queue.')
+    return parser.parse_args()
+
+
+def log(*args, **kwargs) -> None:
+    if not quiet or kwargs.get('file', sys.stdout) != sys.stdout:
+        print(*args, **kwargs)
+
+
+def main() -> int:
+    global quiet
+    args: argparse.Namespace = parse_args()
+    quiet = args.quiet
+
+    with open('credentials.json', encoding='utf-8') as credentials_file:
+        credentials: dict[str, str] = json.load(credentials_file)
+
+    setup_conn()
+    login(credentials)
+
+    log("Waiting for 9pm")
+    while True:
+        if datetime.now().strftime("%H") >= "21":
+            break
+
+    slot: str = get_slot(args.slot, args.backup_slot)
+    if not slot:
+        log('The server is possibly down!', file=sys.stderr)
+        exit(1)
+
+    reserve(args.queue, credentials)
+    log(f'Enjoy your day on {slot.split()[0]}! ;)')
+    return 0
+
+if __name__ == '__main__':
+    exit(main())
